4ad328515fc7e093e2fdd932dec1951aa82df6d7
Ayham-ALMoussallam <ayhamdead@gmail.com>
2025-09-16 17:30:55 +0300

    Removed JetStream

--- routes/web.php
+++ routes/web.php
@@ -7,15 +7,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+
 
 // Show all products
 Route::get('/products', [ProductController::class, 'ShowProductPage'])->name('products');
