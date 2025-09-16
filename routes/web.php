36a9d85c94752afc30ab2a745592b64b38ace051
Ayham-ALMoussallam <ayhamdead@gmail.com>
2025-09-16 17:41:44 +0300

    Breez Installed

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-// Show all products
-Route::get('/products', [ProductController::class, 'ShowProductPage'])->name('products');
-
-// Add a new product
-Route::post('/products/add', [ProductController::class, 'AddProduct'])->name('add-product');
-
-// Update a product (usually via form with PUT method)
-Route::post('/products/update/{id}', [ProductController::class, 'UpdateProduct'])->name('update-product');
-
-// Delete a product
-Route::get('/products/delete/{id}', [ProductController::class, 'DeleteProduct'])->name('delete-product');
-
+require __DIR__.'/auth.php';
