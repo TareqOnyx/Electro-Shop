cd21e7eec17e0a947a805b1aab718a935fabc2d0
Ayham-ALMoussallam <ayhamdead@gmail.com>
2025-09-16 18:49:02 +0300

    Redirect Function

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Product_Controller;
+
+Route::get('/', [UserController::class, 'Redirect']);
+
 
 Route::get('/', function () {
     return view('welcome');
