423e55f9c29289ad35ec373d6b8a02d5542d156c
TareqOnyx <tareqchehab111@gmail.com>
2025-09-17 20:48:00 +0300

    dashboard auth

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,10 @@ Route::get('/', function () {
 
 
 
+Route::middleware(['auth', 'isAdmin'])->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+});
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
