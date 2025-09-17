ee9153d0c3cb7e3676d6fc894b64391b32fcb292
TareqOnyx <tareqchehab111@gmail.com>
2025-09-17 19:51:37 +0300

    1

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,13 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
-=======
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
->>>>>>> 9b9497d10bada0878d995b65772bc2a9adaaea05
 
 Route::get('/', function () {
     return view('index'); // public homepage
 });
 
-<<<<<<< HEAD
-=======
 
 
 // Redirect after login (authenticated users only)
@@ -22,7 +17,6 @@ Route::get('/', function () {
 
 
 
->>>>>>> 9b9497d10bada0878d995b65772bc2a9adaaea05
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
